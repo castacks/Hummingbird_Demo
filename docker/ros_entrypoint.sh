96cae779bf01dfda1139caf362f5c0b7ba5a67da
Tyler Harp <tharp@andrew.cmu.edu>
2024-11-22 18:13:05 -0500

    fixing file

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -3,7 +3,6 @@ set -e
 
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash" --
-source "/root/ros2_ws/install/local_setup.bash" --
 
 # Welcome information
 echo "ZED ROS2 Docker Image"
