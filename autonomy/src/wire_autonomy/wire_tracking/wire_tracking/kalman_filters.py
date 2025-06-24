a54ce860e327cdcffce68e3bc585030d695739dc
Tyler Harp <tkharp789@gmail.com>
2025-06-24 11:55:23 -0400

    going towards a local kalman filter

--- autonomy/src/wire_autonomy/wire_tracking/wire_tracking/kalman_filters.py
+++ autonomy/src/wire_autonomy/wire_tracking/wire_tracking/kalman_filters.py
@@ -42,6 +42,7 @@ class PositionKalmanFilter:
         self.P_init = self.R.copy() * self.inital_cov_multiplier # Initial estimate covariance to account for initial uncertainty
 
         self.max_kf_id = 0
+        self.target_kf_id = None  # ID of the Kalman filter point to track
 
     def add_kf(self, y0, z0):
         """
@@ -58,32 +59,57 @@ class PositionKalmanFilter:
         self.kf_colors = np.vstack((self.kf_colors, self.generate_viz_color()))
         self.valid_counts = np.vstack((self.valid_counts, np.array([[self.valid_count]])))
 
-    def initialize_kfs(self, camera_points, wire_angle):
+    def remove_stale_kfs(self):
+        """
+        Remove a Kalman filter point by its ID.
+        
+        Parameters:
+        - kf_id: ID of the Kalman filter point to remove.
+        """
+        idx = np.where(self.valid_counts < 0)
+        if idx.size > 0:
+            self.kf_ids = np.delete(self.kf_ids, idx, axis=0)
+            self.kf_points = np.delete(self.kf_points, idx, axis=0)
+            self.kf_covariances = np.delete(self.kf_covariances, idx, axis=0)
+            self.kf_colors = np.delete(self.kf_colors, idx, axis=0)
+            self.valid_counts = np.delete(self.valid_counts, idx, axis=0)
+        return len(idx) # Return the number of removed Kalman filter points
+
+    def initialize_kfs(self, camera_points, wire_yaw):
         assert camera_points.shape[1] == 3, f"camera_points shape: {camera_points.shape}"  # Ensure points are in (x, y, z) format, N x 3
 
-        perp_angle = wdu.perpendicular_angle_rad(wire_angle)
-        dists = camera_points[:, 0] * np.cos(perp_angle) + camera_points[:, 1] * np.sin(perp_angle)
+        dists = camera_points[:, 0] * np.sin(wire_yaw) - camera_points[:, 1] * np.cos(wire_yaw)
         self.kf_points = np.hstack((dists.reshape(-1, 1), camera_points[:, 2].reshape(-1, 1)))
         self.kf_ids = np.arange(1, len(camera_points) + 1).reshape(-1, 1)
         self.kf_covariances = np.repeat(self.P_init.reshape(1, 2, 2), len(camera_points), axis=0)
         self.kf_colors = self.generate_viz_color(len(camera_points))
         self.valid_counts = np.ones((len(camera_points), 1), dtype=int)
 
-    def predict(self, relative_H_transform):
+    def predict(self, relative_H_transform, previous_wire_yaw, current_wire_yaw):
         """Predict the state and estimate covariance after a relative transformation."""
+        assert relative_H_transform.shape == (4, 4), f"relative_H_transform shape: {relative_H_transform.shape}"  # Ensure transformation matrix is 4x4
+        # update the kalman points
+        xy_points = self.get_xys_from_dists(self.kf_points[:, 0], previous_wire_yaw)
+        xyz_points = np.hstack((xy_points, self.kf_points[:, 1].reshape(-1, 1)))
+        transformed_points = np.dot(relative_H_transform[:3, :3], xyz_points.T).T + relative_H_transform[:3, 3]
+        assert transformed_points.shape == (self.kf_points.shape[0], 3), f"transformed_points shape: {transformed_points.shape}"  # Ensure transformed points are in (x, y, z) format
+        new_kf_dists = self.get_dists_from_xys(transformed_points[:, :2], current_wire_yaw)
+        self.kf_points[:, 0] = new_kf_dists
+        self.kf_points[:, 1] = transformed_points[:, 2]
 
-        xyz
+        # Update the covariance matrix
         
 
-    def update(self, y, z):
+    def update(self, cam_points, wire_yaw):
         """Update the state estimate using a new measurement."""
-        # Calculate Kalman Gain
-
-        measured_pos = np.array([[y], [z]]).reshape(-1, 1)
-        assert measured_pos.shape == (2, 1), f"measured_pos shape: {measured_pos.shape}"
-
+        assert cam_points.shape[1] == 3, f"cam_points shape: {cam_points.shape}"  # Ensure points are in (x, y, z) format, N x 3
+    
         S = self.P + self.R
         K = self.P @ np.linalg.inv(S)
+
+        measured_dists = self.get_dists_from_xys(cam_points[:, :2], wire_yaw)
+        dh_measured = np.hstack((measured_dists, cam_points[:, 2].reshape(-1, 1)))  # Combine distances and heights
+        assert dh_measured.shape[1] == 2, f"dh_measured shape: {dh_measured.shape}"  # Ensure points are in (distance, height) format
 
         # Update the state estimate and covariance
         y = measured_pos - self.curr_pos
@@ -93,6 +119,15 @@ class PositionKalmanFilter:
         self.P = (np.eye(2) - K) @ self.P
         self.valid_count += 1
 
+    def get_dists_from_xys(xy_points, wire_yaw):
+        dists = xy_points[:, 0] * np.sin(wire_yaw) - xy_points[:, 1] * np.cos(wire_yaw)
+        return dists.reshape(-1, 1)
+    
+    def get_xys_from_dists(dists, wire_yaw):
+        x = dists * -np.sin(wire_yaw)
+        y = dists * np.cos(wire_yaw)
+        return np.column_stack((x, y))
+    
     def generate_viz_color(self, num_colors=1):
         """
         Generate a random color for visualization.
@@ -103,8 +138,9 @@ class PositionKalmanFilter:
         saturation = np.random.randint(int(0.5 * 255), 255, num_colors, dtype=np.uint8)
         value = np.ones(num_colors, dtype=np.uint8) * 255  # Full brightness
         return cv2.cvtColor(np.uint8([[[hue, saturation, value]]]), cv2.COLOR_HSV2RGB)[0][0]
-class YawKalmanFilter:
-    def __init__(self, yaw0, wire_tracking_config):
+    
+class DirectionKalmanFilter:
+    def __init__(self, direction0, wire_tracking_config):
         """
         Initialize the Kalman Filter.
         
@@ -116,38 +152,31 @@ class YawKalmanFilter:
         - x0: Initial state estimate.
         - P0: Initial estimate covariance.
         """
+        assert isinstance(direction0, (list, np.ndarray)), "direction0 must be a list or numpy array"
+        assert len(direction0) == 3, "direction0 must have three elements (vx, vy, vz)"
         self.Q_val = wire_tracking_config['yaw_predict_covariance'] ** 2  # Process noise covariance
         self.R_val = wire_tracking_config['yaw_measurement_covariance'] ** 2  # Measurement noise covariance
         self.max_yaw_covariance = wire_tracking_config['max_yaw_covariance'] ** 2  # Maximum yaw covariance
         self.inital_cov_multiplier = wire_tracking_config['initial_yaw_covariance_multiplier']
 
-        self.curr_state = np.array([np.cos(yaw0), np.sin(yaw0)]).reshape(-1, 1)
-        self.valid_count = 1
+        self.R = np.eye(3) * self.R_val  # Measurement noise covariance matrixq
+        self.Q = np.eye(3) * self.Q_val  # Process noise covariance matrix
 
-        angle_jacobian = self.get_angle_jacobian_squared(yaw0)
-        self.P = self.inital_cov_multiplier * self.R_val * angle_jacobian
-        
-    def get_angle_jacobian_squared(self, yaw): # effecitvely J @ J.T
-        """
-        Calculate the Jacobian of the angle.
-        
-        Returns:
-        - angle_jacobian: Jacobian matrix of the angle.
-        """
-        return np.array([[np.sin(yaw) ** 2, -np.sin(yaw) * np.cos(yaw)],
-                         [-np.sin(yaw) * np.cos(yaw), np.cos(yaw) ** 2]])
+        self.curr_direction = np.array(direction0).reshape(-1, 1)  # Current direction vector
+        self.P = self.inital_cov_multiplier * self.R  # Initial estimate covariance
 
-    def predict(self, relative_yaw_change):
+    def predict(self, relative_rotation):
         """
         Predict the state and estimate covariance.
         """
+        assert relative_rotation.shape == (3, 3), f"relative_rotation shape: {relative_rotation.shape}"  # Ensure rotation matrix is 3x3
         # State prediction
-        F = np.array([[np.cos(relative_yaw_change), -np.sin(relative_yaw_change)],
-                      [np.sin(relative_yaw_change), np.cos(relative_yaw_change)]])
-        self.curr_state = F @ self.curr_state
-        self.P = F @ self.P @ F.T + self.Q_val * self.get_angle_jacobian_squared(self.get_yaw())
+        self.curr_direction = relative_rotation @ self.curr_direction
+        self.curr_direction /= np.linalg.norm(self.curr_direction)  # Normalize the direction vector
+        assert self.curr_direction.shape == (3, 1), f"curr_direction shape: {self.curr_direction.shape}"
+        self.P = relative_rotation @ self.P @ relative_rotation.T + self.Q
 
-    def update(self, measured_yaw):
+    def update(self, measured_direction):
         """
         Update the state estimate using a new measurement.
         
@@ -155,40 +184,19 @@ class YawKalmanFilter:
         - measured_yaw: float, yaw angle in radians
         """
         # Compute angle difference between measured and predicted yaw
-        predicted_yaw = self.get_yaw()
-        dyaw = (measured_yaw - predicted_yaw)
-
-        # Convert dyaw back to vector form (unit vector)
-        residual = np.array([np.cos(dyaw), np.sin(dyaw)])
-
-        # Kalman gain
-        R = self.get_angle_jacobian_squared(measured_yaw) * self.R_val
-        S = self.P + R
-        K = self.P @ np.linalg.inv(S)
-
-        # Update state
-        self.curr_state = self.curr_state + K @ residual
-
-        # Update covariance
-        self.P = (np.eye(2) - K) @ self.P
-
-    def update(self, measured_yaw):
-        """
-        Update the state estimate using a new measurement.
         
-        Parameters:
-        - z: Measurement vector.
-        """
-        # Calculate Kalman Gain
-        R = self.get_angle_jacobian_squared(measured_yaw) * self.R_val
-        S = self.P + R
+        S = self.P + self.R
         K = self.P @ np.linalg.inv(S)
 
-        # Update the state estimate and covariance
-        measured_yaw = np.array([np.cos(measured_yaw), np.sin(measured_yaw)])
-        y = measured_yaw - self.curr_state
-        self.curr_state = self.curr_state + K @ y
-        self.P = (np.eye(2) - K) @ self.P
+        measured_direction = np.array(measured_direction).reshape(-1, 1)
+        assert measured_direction.shape == (3, 1), f"measured_direction shape: {measured_direction.shape}"
+        y = measured_direction - self.curr_direction
+        assert y.shape == (3, 1), f"y shape: {y.shape}"
+        self.curr_direction = self.curr_direction + K @ y
+        self.curr_direction /= np.linalg.norm(self.curr_direction)  # Normalize the direction vector
+        assert self.curr_direction.shape == (3, 1), f"curr_direction shape: {self.curr_direction.shape}"
+
+        self.P = (np.eye(3) - K) @ self.P
     
     def get_yaw(self):
-        return np.arctan2(self.curr_state[1], self.curr_state[0])
+        return np.arctan2(self.curr_direction[1, 0], self.curr_direction[0, 0])
