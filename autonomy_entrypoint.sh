074ca48d2999c61bb410d0db2c74bf0ea8e75902
Tyler Harp <tharp@andrew.cmu.edu>
2025-08-04 17:11:55 -0400

    adding an entry point for autonomy

--- autonomy_entrypoint.sh
+++ autonomy_entrypoint.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-
 set -e
+
+source /root/dependencies_ws/install/setup.bash
+
+if [ ! -d '/root/ros2_ws/install' ]; then
+  source ~/.bash_profile
+  colcon build --symlink-install --parallel-workers $(nproc) --cmake-args -Wno-dev
+else
+  echo "Already built"
+fi
+
+source /root/ros2_ws/install/setup.bash
+
+# This is the critical part: run ros2 launch as the main process
+exec ros2 launch system_bringup bringup.launch.py
