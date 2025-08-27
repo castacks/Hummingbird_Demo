bfc229b5735cc2ab1440936d9a80e4861eb93026
Tyler Harp <tharp@andrew.cmu.edu>
2025-08-27 16:25:07 -0400

    changing address of stream

--- gcs/play_stream.sh
+++ gcs/play_stream.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
 set -e
 
-gst-launch-1.0 -v rtspsrc location=rtsp://127.0.0.1:8554/wire_tracking latency=0 ! rtph264depay ! avdec_h264 ! videoconvert ! autovideosink sync=false
+gst-launch-1.0 -v rtspsrc location=rtsp://10.223.28.111:8554/wire_tracking latency=0 ! rtph264depay ! avdec_h264 ! videoconvert ! autovideosink sync=false
